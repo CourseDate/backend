f158804b904a86bc24e8b3b4fc9d551ab212bf5a
Ka Wong <kawong2020@u.northwestern.edu>
2020-03-03 17:39:43 -0600

    update schema with rest of stuff

--- src/resolver.js
+++ src/resolver.js
@@ -1,8 +1,12 @@
+const exampleUser = { id: 123, year: "FRESHMAN", interest_dept: ["GEN_ENG"], cart: [] };
+
+
 module.exports = {
   Query: {
     me: () => {
       console.log("resolver called");
-      return { id: 123, year: 1, interest_dept: ["GEN_ENG"], cart: [] };
+      return { id: 123, year: "FRESHMAN", interest_dept: ["GEN_ENG"], cart: [] };
     }
+
   }
 };
--- src/schema.js
+++ src/schema.js
@@ -4,12 +4,41 @@ const typeDefs = gql`
   type Query {
     me: User
   }
+
   type User {
-    id: Int!
-    year: Int!
+    id: ID!
+    year: SchoolYear
     interest_dept: [String]!
     cart: [Int]!
   }
+
+  type Activity {
+    id: ID!
+    userId: Int!
+    classId: Int!
+    isLiked: Boolean!
+    dateCreated: Date!
+  }
+
+  type Classes {
+    id: ID
+    classCode: String!
+    classNo: Int!
+    name: String!
+    description: String!
+    dept: String!
+    school: String!
+    prereqs: String!
+  }
+
+  enum SchoolYear {
+    FRESHMAN
+    SOPHOMORE
+    JUNIOR
+    SENIOR
+  }
+
+  scalar Date
 `;
 
 module.exports = typeDefs;
