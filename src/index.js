001a3f66acd8879ad6ea19d8f6d34188d9c833c6
Ka Wong <kawong2020@u.northwestern.edu>
2020-03-03 17:16:35 -0600

    it works

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 const { ApolloServer, gql } = require("apollo-server");
-const typeDefs = gql`
-  type Query {
-    me: User
-  }
-  type User {
-    id: Int!
-    year: Int!
-    interest_dept: [String]!
-    cart: [Int]!
-  }
-`;
-const books = [
-  {
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K. Rowling"
-  },
-  {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
-  }
-];
+const typeDefs = require("./schema");
+const resolvers = require("./resolver");
 
-const sample = { id: 123, year: 1, interest_dept: ["GEN_ENG"], cart: [] };
-const resolvers = {
-  Query: {
-    me: () => sample
-  }
-};
 const server = new ApolloServer({ typeDefs, resolvers });
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
